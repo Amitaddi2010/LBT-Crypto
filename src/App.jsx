b25f2091a3940173c475226fd8aed6651324746b
Darkcheftar <darkcheftar.connect@gmail.com>
2022-05-23 19:39:34 +0530

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,41 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import logo from './imgs/logo.png'
-import { useState } from 'react';
+import Fancylogo from './components/Fancylogo/Fancylogo';
+import { useState, useEffect } from 'react';
 import Lottery from './components/Lottery/Lottery';
 import Swap from './components/Swap/Swap';
-import Aboutus from './components/About us/AboutUs';
 import Footer from './components/Footer/Footer'
 import Timeline from './components/Timeline/Timeline'
 import Team from './components/Team/Team'
- 
- 
+
+
 function App() {
+  
   const LotteryShow = 1;
   const SwapShow = 2;
-  const AboutShow =3;
   let [show, setShow] = useState(2);
 
   return (
     <div className="App">
-      
-      <>
-      <div className='logo'>
-        <img className='mainlogo' src={logo} alt =""/>
-      </div>
-      </>
-      
-      {/* <img src={sample} alt="" /> */}
-      <Navbar handler={setShow} />
-      <hr/>
-      
+      <Navbar handler={setShow} show={show}/>
       <div className='container'>
-      <div id="space">
+        <div id="space">
+          <Fancylogo />
+        </div>
+        <div className='main'>
+          {show === LotteryShow && <Lottery />}
+          {show === SwapShow && <Swap />}
+        </div>
+      </div>
 
-    <div class="elogo">
-           <div class="trif u1"></div>
-           <div class="trif u2"></div>
-           <div class="trif u3"></div>
-           <div class="trif u4"></div>
-           <div class="ct"></div>
-           <div class="trif l1"></div>
-           <div class="trif l4"></div>
-            
+            <Timeline />
+       
+          <Team />
+          <Footer />
+        
+
     </div>
-    <hr  class="hr-1"/>
-    <div className='Timeline'><Timeline/></div>
-    <hr/>
-    <Team/>
-    <br></br>
-    <div><Footer/></div> 
-   </div>
-  
 
-
-      {/* <img className="cryptogif" src={sample} alt="" /> */}
-      <div className='main'>
-      {show===LotteryShow && <Lottery/>}
-      {show===SwapShow && <Swap/>}
-      {show===AboutShow&& <Aboutus/>}
-      
-    <br></br>
-    <br></br>
-    
-    {/* <div id="canvas_container"></div> */}
-   
-      </div> </div>
-      
-    </div>
-    
   );
 }
 
