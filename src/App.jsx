f0a63a541b6337092e4592690c5930fa41298a9b
Darkcheftar <darkcheftar.connect@gmail.com>
2022-06-09 22:05:03 +0530

    Style re-thinked

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import Fancylogo from './components/Fancylogo/Fancylogo';
 import { useState, useEffect } from 'react';
 import Lottery from './components/Lottery/Lottery';
 import Swap from './components/Swap/Swap';
@@ -8,39 +7,42 @@ import Footer from './components/Footer/Footer'
 import Timeline from './components/Timeline/Timeline'
 import Team from './components/Team/Team'
 import NFT from './components/NFT/NFT';
+import Home from './components/Home/Home';
 import Metaverse from './components/Metaverse/Metaverse';
-import {loadAccount, loadWeb3, loadContract, buyTokens, sellTokens, loadBalance} from './utils/utils';
+import { loadAccount, loadWeb3, loadContract, buyTokens, sellTokens, loadBalance } from './utils/utils';
+
+import { Routes, Route, Link } from "react-router-dom";
 
 
 function App() {
   const SwapShow = 2;
   const LotteryShow = 1;
-
+  const [appClass, setAppClass] = useState(false);
   const [show, setShow] = useState(SwapShow);
   const [account, setAccount] = useState(null);
   const [balanceAsEther, setbalanceAsEther] = useState();
   const [tokenBal, setTokenBal] = useState(0);
   const [loading, setLoader] = useState(false);
   const [manager, setManager] = useState('');
-  const [lotteryContract,setLotteryContract] = useState();
-  const [lotteryAddress,setLotteryAddress] = useState("");
-  useEffect(()=>{
+  const [lotteryContract, setLotteryContract] = useState();
+  const [lotteryAddress, setLotteryAddress] = useState("");
+  useEffect(() => {
     loadWeb3();
     loadAccount(setAccount);
-    loadContract({setManager, setLotteryContract, setLotteryAddress});
-  },[])
+    loadContract({ setManager, setLotteryContract, setLotteryAddress });
+  }, [])
   useEffect(() => {
     window.ethereum?.on("accountsChanged", handleAccountChange);
     return () => {
       window.ethereum?.removeListener("accountsChanged", handleAccountChange);
     };
   });
-  useEffect(()=>{
-    loadBalance({account, setbalanceAsEther,setTokenBal });
-  },[account,loading])
+  useEffect(() => {
+    loadBalance({ account, setbalanceAsEther, setTokenBal });
+  }, [account, loading])
   const handleAccountChange = (...args) => {
     // you can console to see the args
-    const accounts = args[0] ;
+    const accounts = args[0];
     // if no accounts that means we are not connected
     if (accounts.length === 0) {
       console.log("Please connect to metamask");
@@ -54,49 +56,29 @@ function App() {
     }
   };
   return (
-
-    <div className="App">
-     {/* <div className = "loader-wrapper"> 
-<span ClassName= "loader" ><span className= "loader-inner" ></span> </span>
-</div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script>
-  $(window).on("load",function(){
-    $(".loader-wrapper").fadeOut("slow")
-  })
-</script> */}
-      <Navbar handler={setShow} show={show} account={account}/>
-     
-      <div className='container'>    
-        <div id="space">
-          <Fancylogo />
-        </div>
-        {!loading?
-        <div className='main'>
-          {show === LotteryShow && <Lottery manager={manager} account={account} setLoader={setLoader}/>}
-          {show === SwapShow && 
-          <Swap  
+    <div className={appClass ? 'App Landing':'App' }>
+      <Navbar handler={setShow} show={show} account={account} />
+      <div className='container'>
+        <Routes>
+          <Route path="/" element={<Home setAppClass={setAppClass} />} />
+          <Route path="/lottery" element={<Lottery manager={manager} account={account} setLoader={setLoader} />} />
+          <Route path="/swap" element={<Swap
             etherBalance={balanceAsEther}
             tokenBalance={tokenBal}
-            buyTokens={async (amount)=>{
+            buyTokens={async (amount) => {
               await buyTokens(amount, setLoader, account);
             }}
-            sellTokens={async (amount)=>{
+            sellTokens={async (amount) => {
               await sellTokens(amount, setLoader, account);
-              
             }}
-            />}
-        </div>:<div>loading...</div>}
+          />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="timeline" element={<Timeline />} />
+          <Route path="/nft" element={<NFT />} />
+          <Route path="/metaverse" element={<Metaverse />} />
+        </Routes>
       </div>
-      <Team />  
-      <Timeline />
-<NFT/>
- 
-<Metaverse/>
-           
-<Footer />
-        
-         
+      <Footer />
     </div>
 
   );
--- src/components/Metaverse/Metaverse.jsx
+++ src/components/Metaverse/Metaverse.jsx
@@ -3,7 +3,7 @@ import './Metaverse.css'
 import Meta2 from './../../imgs/Metaverse-img/meta2.png'
 export default function Metaverse() {
   return (
-    <div id= "Metaverse" className='Meta'>
+    <div id= "Metaverse" className='metaverse'>
   <h1 className="heading1"><span>Our</span>Metaverse</h1>
   <h2>Metaverse 3D space development</h2>
   <img className='Metaverse1' src={Meta2} alt=""/>
